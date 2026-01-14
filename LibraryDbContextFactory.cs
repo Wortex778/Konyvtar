5ed32da19ad5fe3a72449edd6cd99fbac0b690c2
Kovacs Botond <21.kovacs.botond@mechwart.com>
2026-01-14 01:21:57 +0100

    adatbazis inicializalasa , DTO es AutoMapper hozzaadasa

---
+++ LibraryDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Konyvtar.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Konyvtar
+{
+    public class LibraryDbContextFactory : IDesignTimeDbContextFactory<LibraryDbContext>
+    {
+        public LibraryDbContext CreateDbContext(string[] args)
+        {
+            // Konfiguráció beolvasása
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<LibraryDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlite(connectionString);
+
+            return new LibraryDbContext(optionsBuilder.Options);
+        }
+    }
+}
